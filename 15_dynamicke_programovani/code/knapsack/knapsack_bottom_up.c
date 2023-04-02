49da340c4f2aef35e627205fa0a11ce135d14a9b
Matouš Volf <66163112+matous-volf@users.noreply.github.com>
2023-04-02 20:12:05 +0200

    Pridan vypis itemu v bottom up reseni

--- 15_dynamicke_programovani/code/knapsack/knapsack_bottom_up.c
+++ 15_dynamicke_programovani/code/knapsack/knapsack_bottom_up.c
@@ -4,64 +4,95 @@
 typedef unsigned int ui;
 
 typedef struct {
+    ui value;
+    ui preWeight;
+} MemoCell;
+
+typedef struct {
     ui weight;
     ui cost;
 } Item;
 
 ui max(ui a, ui b) { return (a > b) ? a : b; }
 
-
-ui knapsack_bottom_up(ui maxWeight, Item *items, ui numberOfItems, ui **memo) {
+ui knapsack_bottom_up(ui maxWeight, Item *items, ui numberOfItems, MemoCell **memo) {
     if (numberOfItems < 1) return 0;
 
     for (int i = 0; i < numberOfItems; i++) {
         Item item = items[i];
         for (int j = 0; j <= maxWeight; j++) {
+            MemoCell *memoCell = &memo[i + 1][j];
+
             // This item's weight is bigger
             // than Knapsack capacity
             // This combination cannot be in the optimal solution
             // Keep the knapsack as is, without this item
             if (item.weight > j) {
-                memo[i + 1][j] = memo[i][j];
+                memoCell->value = memo[i][j].value;
+                memoCell->preWeight = j;
                 continue;
             }
 
-            memo[i + 1][j] = max(
-                    // Option 1: add this item into knapsack
-                    // -- Add cost of this item and cost of the rest of the knapsack
-                    item.cost + memo[i][j - item.weight],
-                    // Option 2: throw away this item
-                    // -- Do not add cost of this item
-                    memo[i][j]
-            );
+            // Option 1: add this item into knapsack
+            // -- Add cost of this item and cost of the rest of the knapsack
+            ui addCost = item.cost + memo[i][j - item.weight].value;
+            // Option 2: throw away this item
+            // -- Do not add cost of this item
+            ui dontAddCost = memo[i][j].value;
+
+            // Take the larger value of these two options and assign the previous weight
+            if (addCost > dontAddCost) {
+                memoCell->value = addCost;
+                memoCell->preWeight = j - item.weight;
+            } else {
+                memoCell->value = dontAddCost;
+                memoCell->preWeight = j;
+            }
         }
     }
 
-    return memo[numberOfItems][maxWeight];
+    return memo[numberOfItems][maxWeight].value;
 }
 
 ui knapsack(ui maxWeight, Item *items, ui numberOfItems) {
     // 2D array | matrix
-    ui **memo = (ui **) malloc((numberOfItems + 1) * sizeof(ui *));
+    MemoCell **memo = (MemoCell **) malloc((numberOfItems + 1) * sizeof(MemoCell *));
 
     for (int i = 0; i < numberOfItems + 1; i++) {
-        memo[i] = (ui *) malloc((maxWeight + 1) * sizeof(ui));
+        memo[i] = (MemoCell *) malloc((maxWeight + 1) * sizeof(MemoCell));
     }
 
     // initialize table to all 0
     for (int i = 0; i < numberOfItems + 1; i++) {
         for (int j = 0; j < maxWeight + 1; j++) {
-            memo[i][j] = 0;
+            memo[i][j].value = 0;
+            memo[i][j].preWeight = 0;
         }
     }
 
     ui result = knapsack_bottom_up(maxWeight, items, numberOfItems, memo);
 
+    // Memo debug print
     for (int i = 0; i <= numberOfItems; i++) {
         for (int j = 0; j <= maxWeight; j++) {
-            printf("%8d", memo[i][j]);
+            printf("%8d", memo[i][j].value, memo[i][j].preWeight);
         }
         printf("\n");
+    }
+
+    // Items list print
+    printf("Items to add:\n");
+
+    ui currentWeight = maxWeight;
+    for (ui i = numberOfItems; i > 0; i--) {
+        MemoCell current = memo[i][currentWeight];
+
+        if (currentWeight > current.preWeight) {
+            // The previous weight is lower - the item was added
+            printf("%u\n", i);
+        }
+
+        currentWeight = current.preWeight;
     }
 
     return result;
@@ -89,9 +120,9 @@ int main() {
 
     for (int i = 0; i < numberOfItems; i++) {
         Item *item = items + i;
-        scanf("%d %d", &item->weight, &item->cost);
+        scanf("%u %u", &item->weight, &item->cost);
     }
 
-    printf("%u\n", knapsack(maxWeight, items, numberOfItems));
+    printf("The total cost: %u\n", knapsack(maxWeight, items, numberOfItems));
     return 0;
 }
